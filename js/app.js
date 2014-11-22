b20438919134c3bb5a1e6c4772fe082c0f567b15
lukesmith123 <lukesmith@bukowski.net>
2014-11-22 20:59:09 +0000

    latest

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var db;
+var dbe;
+var dbc;
+var dbl;
 
 function loadData(pg_id) {
 $.ajax({ 
@@ -7,14 +9,17 @@ $.ajax({
 		data: {page_id: pg_id }, 
 		success: function(data) { 
 
-		db = $.parseJSON(data);
+		
 		if(pg_id == 1){ //EVENTS
+			dbe = $.parseJSON(data);
 			parseEvents();
 		}
-		else if(pg_id == 3) {
+		else if(pg_id == 3) { //CREWS
+			dbc = $.parseJSON(data);
 			parseCrews();
 		}
-		else if(pg_id == 4) {
+		else if(pg_id == 4) { //LESSONS
+			dbl = $.parseJSON(data);
 			parseLessons();
 		}
 	 }, 
@@ -26,12 +31,14 @@ $.ajax({
 }
 
 function parseEvents(){
-	for (var i = 0, len = db.length; i < len; i++) {
+	for (var i = 0, len = dbe.length; i < len; i++) {
 	  var str="";
-	  str += '<li><a href="#"><h1>';
-	  str += db[i].name;
+	  str += '<li><a href="#" onclick="loadNextPage(1,';
+	  str += i;
+	  str += ');"><h1>';
+	  str += dbe[i].name;
 	  str += '</h1><p>';
-	  str += db[i].date;
+	  str += dbe[i].date;
 	  str += '</p></a></li>';
 	  $('#liste').append(str);
   }
@@ -39,12 +46,14 @@ function parseEvents(){
 }
 
 function parseCrews() {
-	for (var i = 0, len = db.length; i < len; i++) {
+	for (var i = 0, len = dbc.length; i < len; i++) {
 	  var str="";
-	  str += '<li><a href="#"><h1>';
-	  str += db[i].name;
+	  str += '<li><a href="#" onclick="loadNextPage(3,';
+	  str += i;
+	  str += ');"><h1>';
+	  str += dbc[i].name;
 	  str += '</h1><p>';
-	  str += db[i].location;
+	  str += dbc[i].location;
 	  str += '</p></a></li>';
 	  $('#listc').append(str);
   }
@@ -52,22 +61,39 @@ function parseCrews() {
 }
 
 function parseLessons() {
-	for (var i = 0, len = db.length; i < len; i++) {
+	for (var i = 0, len = dbl.length; i < len; i++) {
 	  var str="";
-	  str += '<li><a href="#"><h1>';
-	  str += db[i].location;
+	  str += '<li><a href="#" onclick="loadNextPage(4,';
+	  str += i;
+	  str += ');"><h1>';
+	  str += dbl[i].location;
 	  str += '</h1><p>';
-	  str += db[i].instructor;
+	  str += dbl[i].instructor;
 	  str += '</p><p>';
-	  str += db[i].level;
+	  str += dbl[i].level;
 	  str += '</p></a></li>';
 	  $('#listl').append(str);
   }
   $("#listl").listview("refresh");
 }
 
-function loadData2()
-{
-	alert('out');
-}
+function loadNextPage(pg_id, index) {
+	if(pg_id == 1) {
+		var mywebpage = '<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>';
 
+		element = document.getElementById('dom-id');  
+		element.innerHTML = mywebpage;
+	}
+	else if (pg_id == 3) {
+		var mywebpage = '<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>';
+
+		element = document.getElementById('dom-id');  
+		element.innerHTML = mywebpage;
+	}
+	else if (pg_id == 4) {
+		var mywebpage = '<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>';
+
+		element = document.getElementById('dom-id');  
+		element.innerHTML = mywebpage;
+	}
+}
